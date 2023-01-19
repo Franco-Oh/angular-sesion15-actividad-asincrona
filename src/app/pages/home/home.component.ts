fb3769f2038767a6b7fa4fbfa6fbf065b587ffb6
Franco Ojeda <fojedavideos@gmail.com>
2023-01-19 00:38:27 -0500

    implementacion de boton para eliminar y aviso de regsitro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,24 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  sup = [
-    {
-      nombre: "Luis",
-      apellido: "Llanos",
-      carrera: "Ing. Sistemas"
-    },
-    {
-      nombre: "Jose",
-      apellido: "Perez",
-      carrera: "Ing. Redes"
-    },
-    {
-      nombre: "Pepe",
-      apellido: "Lopez",
-      carrera: "Ing. Electrónico"
+  // Variables de formulario
+  nombre1!:string;
+  apellido1!:string;
+  carrera1!:string;
+  agregarDatos!:any;
+
+  msgAgregado = false;
+  msgEliminado = false;
+
+  // Base de datos
+  registrados = [
+  {
+    nombre: "Luis",
+    apellido: "Llanos",
+    carrera: "Ing. Sistemas"
+  },
+  {
+    nombre: "Jose",
+    apellido: "Perez",
+    carrera: "Ing. Redes"
+  },
+  {
+    nombre: "Pepe",
+    apellido: "Lopez",
+    carrera: "Ing. Electrónico"
+  }
+  ];
+
+  // data = Object.values(this.people.indexOf);
+  
+  // Funcion para agregar personas a base de datos
+  insertarDatos(){ 
+    this.agregarDatos = {
+      nombre: this.nombre1,
+      apellido: this.apellido1,
+      carrera: this.carrera1
     }
-  ]
-  constructor() { }
+    console.log(this.agregarDatos);
+    this.registrados.push(this.agregarDatos);
+    console.log(this.registrados);
+
+    // Mensaje de exito
+    this.msgAgregado = true;
+    setTimeout(() => {
+      this.msgAgregado = false;
+    }, 3000)
+  }
+
+  // Funcion para eliminar dato especifico
+  eliminar(dato:any){
+    if(confirm('Seguro que desea eliminar este dato?')){
+      var indice = this.registrados.indexOf(dato);
+      console.log(indice);
+      this.registrados.splice(indice, 1);
+
+      // Mensaje de exito
+      this.msgEliminado = true;
+      setTimeout(() => {
+        this.msgEliminado = false;
+      }, 2000)
+    }
+  }
+
+  constructor() {  }
 
   ngOnInit(): void {
   }
